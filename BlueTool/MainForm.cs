fddf6d4a77dee75284f88c88d17af9eeba7c405d
Daniel <790119+DanTheMan827@users.noreply.github.com>
2020-03-15 20:42:12 -0500

    Add License Info

--- BlueTool/MainForm.cs
+++ BlueTool/MainForm.cs
@@ -6,6 +6,25 @@ using System.Linq;
 using System.Text.RegularExpressions;
 using System.Windows.Forms;
 
+/*
+    Copyright 2020 DanTheMan827
+
+    This file is part of BlueTool.
+
+    BlueTool is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    BlueTool is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with BlueTool.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
 namespace com.DanTheMan827.BlueTool
 {
     public partial class MainForm : Form
